fa2d1fed0cc65c9d06abd14c96e9498ae72f4f3c
macaury carvalho <carvalhomacaury@gmail.com>
2023-02-10 13:22:59 -0300

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,20 +9,37 @@ function home() {
   return (
     <>
       <Container>
-        <Row>
+        <Row id="Home">
           <Col>
             <div className="box_home">
               <h1>Introducing the Best Pest Control Agency in New York</h1>
-              <span>Get a personalized quote today.</span>
-              <Button>Get a quote</Button>
+              <span>Get a personalized quote today. <br/> </span>
+              <Button className="btn_get_quote">Get a quote</Button>
             </div>
           </Col>
           <Col>
             <img
-              src="https://raw.githubusercontent.com/macaury/GoodPoison/538d29719403095bc91ebd08283a7bce2c4eaf7e/src/assets/home/home.svg"
+              className="svg_home"
+              src="https://raw.githubusercontent.com/macaury/GoodPoison/b621de64894ef2a6225033c1d4ef1532ebbe7d01/src/assets/home/home.svg"
               alt=""
             />
           </Col>
+          <div className="line_home">
+            <Row>
+              <Col xl="3">
+                <div className="rectangle_project">
+                  <h2>3000+</h2>
+                  <span>Project Done </span>
+                </div>
+              </Col>
+              <Col>
+                <div className="rectangle_client">
+                  <h2>120+</h2>
+                  <span>Happy Client</span>
+                </div>
+              </Col>
+            </Row>
+          </div>
         </Row>
       </Container>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./components/main.css"
+import "./components/home.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
