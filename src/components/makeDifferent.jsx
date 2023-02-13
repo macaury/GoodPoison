2b2b81e7ced4f473fe150c4c92f7ccb80cad8390
macaury carvalho <carvalhomacaury@gmail.com>
2023-02-13 16:32:29 -0300

    update weed control

--- src/components/makeDifferent.jsx
+++ src/components/makeDifferent.jsx
@@ -19,31 +19,26 @@ function makeDifferent() {
               nisl sapien amet, eget semper in. Tempus porta justo vulputate
               sagittis vulputate accumsan vitae, neque, ornare. <br />
             </span>
-            <a> Explore what makes Us different </a>
+            <a> Explore what makes Us different  <img src="" alt="" /> </a>
           </Col>
           <Col xl="5" className="different_col">
             <Row>
               <Col>
                 <h2>120+</h2>
-                <br />
                 <span>Years in Business</span>
               </Col>
               <Col>
-                <h2>
-                  400+ 
-                </h2>
+                <h2>400+</h2>
                 <span>Locations Coverd</span>
               </Col>
             </Row>
             <Row>
               <Col>
                 <h2>160+</h2>
-                <br />
                 <span>Hours of Training</span>
               </Col>
               <Col>
                 <h2>80+</h2>
-                <br />
                 <span>Support Staff</span>
               </Col>
             </Row>
