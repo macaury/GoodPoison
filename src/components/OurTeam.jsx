9d4e116471d354c49bdb7bc963b314edc3392e57
macaury carvalho <carvalhomacaury@gmail.com>
2023-02-13 17:56:17 -0300

    update

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,20 +8,57 @@ import Card from "react-bootstrap/Card";
 
 import "./OurTeam.scss";
 
-
-
 function OurTeam() {
-  return <>
+  const [data, setData] = useState([]);
+  const carousel = useRef();
 
-  <Container>
-    <Row>
-        <div className="ourteam_txt">
+  useEffect(() => {
+    fetch("http://localhost:5173/public/OurTeam.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  return (
+    <>
+      <Container id="OurTeam">
+        <Row>
+          <div className="ourteam_txt">
             <h3>Our Services</h3>
-            <h1>What our team do for you . . .</h1>
-        </div>
-    </Row>
-  </Container>
-  </>;
+            <h1>What our team do for you ...</h1>
+          </div>
+        </Row>
+        <Row>
+          <div className="services" ref={carousel}>
+            {data.map((item) => {
+              const { id, title, image } = item;
+
+              return (
+                <Col className="service_card" key={id}>
+                  <Card.Img className="image" src={image} alt={title} />
+                  <div className="info">
+                    <h3>{title}</h3>
+                    <span>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Pellentesque morbi donec lacus ut amet, scelerisque
+                      ornare.
+                      <br />
+                    </span>
+                    <a className="learn_more" href="#">
+                      Learn More
+                      <img
+                        src="https://raw.githubusercontent.com/macaury/GoodPoison/2b2b81e7ced4f473fe150c4c92f7ccb80cad8390/src/assets/weedControl/linkSet.svg"
+                        alt=""
+                      />
+                    </a>
+                  </div>
+                </Col>
+              );
+            })}
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default OurTeam;
